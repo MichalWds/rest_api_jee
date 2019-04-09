3521735ccf1779fe6738282e46dc7513c77509e0
MichalWds <michalwadas@onet.pl>
2019-04-09 19:08:43 +0200

    add

--- src/main/java/rest_api/UserController.java
+++ src/main/java/rest_api/UserController.java
@@ -1,12 +1,33 @@
 package rest_api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 
+
+
+@Path("users")
 public class UserController {
 
- public List<User> getUsers(){
 
-     return UserDao.getUsers();
+ @Path("all")
+ @GET
+ @Produces(MediaType.APPLICATION_JSON)
+ public String getUsers() throws JsonProcessingException {
+
+     ObjectMapper objectMapper = new ObjectMapper();
+     objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);   //ustawienie serializacji objektow java na json
+     return objectMapper.writeValueAsString(UserDao.getUsers());
+
+
+
 
  }
 
